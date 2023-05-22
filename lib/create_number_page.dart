924bc2d49097d63fe86edee7b3103acacf9f4985
草野紅葉 <kusanomomiji@kusanokouyounoMacBook-Air.local>
2023-05-23 02:09:05 +0900

    登録時のデザイン

--- lib/create_number_page.dart
+++ lib/create_number_page.dart
@@ -28,39 +28,258 @@ class CreateNumberState extends State<CreateNumberPage> {
         appBar: AppBar(
           elevation: 0,
           bottom: PreferredSize(
-            preferredSize: Size.fromHeight(0),
+            preferredSize: const Size.fromHeight(0),
             child: Container(
-              color: Colors.white, // 背景色と同じにすることで線を消す
+              color: Colors.white,
             ),
           ),
           backgroundColor: Colors.white,
           iconTheme: const IconThemeData(color: Colors.black),
           title: const Text('連絡先追加', style: TextStyle(color: Colors.black)),
         ),
-        body: Container(
-          child: Column(children: [
-            Container(
-                alignment: Alignment.topCenter,
-                child: const Icon(
-                  Icons.account_circle,
-                  size: 100,
-                  color: Colors.blue,
-                )),
-            const Divider(
-              height: 5,
-              thickness: 0.3,
-              color: Colors.black,
-            ),
-            Row(
-              children: [TextField()],
-            )
-          ]),
-        )
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(children: [
+              Container(
+                  alignment: Alignment.topCenter,
+                  child: const Icon(
+                    Icons.account_circle,
+                    size: 100,
+                    color: Colors.blue,
+                  )),
+              const Divider(
+                height: 5,
+                thickness: 0.3,
+                color: Colors.black,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                              text: "姓", style: TextStyle(color: Colors.black)),
+                          TextSpan(
+                            text: "（必須）",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 130),
+                    child: RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                              text: "姓フリガナ",
+                              style: TextStyle(color: Colors.black)),
+                          TextSpan(
+                            text: "（必須）",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.teal)),
+                      ),
+                    ),
+                  )),
+                  Flexible(
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.teal)),
+                          ),
+                        )),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                              text: "名", style: TextStyle(color: Colors.black)),
+                          TextSpan(
+                            text: "（必須）",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 130),
+                    child: RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                              text: "名フリガナ",
+                              style: TextStyle(color: Colors.black)),
+                          TextSpan(
+                            text: "（必須）",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.teal)),
+                    )),
+                  )),
+                  Flexible(
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.teal)),
+                          ),
+                        )),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                                text: "電話番号",
+                                style: TextStyle(color: Colors.black)),
+                            TextSpan(
+                              text: "（必須）",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(children: [
+                Flexible(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal)),
+                  )),
+                ))
+              ]),
+              Row(
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                                text: "メールアドレス",
+                                style: TextStyle(color: Colors.black)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(children: [
+                Flexible(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal)),
+                  )),
+                ))
+              ]),
+              Row(
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                                text: "メモ",
+                                style: TextStyle(color: Colors.black)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(children: [
+                Flexible(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 50),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal)),
+                  )),
+                ))
+              ]),
+              Row(children: [
+                Flexible(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all(
+                            const Size(400, 40)), // サイズを指定
+                      ),
+                      child: const Text('完了')),
+                ))
+              ]),
+            ]))
         /*
       Container(
         alignment: Alignment.topCenter,
         child: Icon(
-          Icons.account_circle,
+          Icons.account_circle, 
           size: 100,
           color: Colors.blue,
         ),
